a1a7f454d1e4264217375cbd4579f9c1f49251f0
anujbarval <anujbarwal2000@gmail.com>
2022-07-10 17:40:12 +0530

    RMX1921: Cherish-ify!
    Signed-off-by: Anujbarwal <anujbarwal2000@gmail.com>

---
+++ cherish_RMX1921.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Cherish stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+
+# Official-ify
+CHERISH_BUILD_TYPE := OFFICIAL
+
+# Inherit from RMX1921 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := RMX1921
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_NAME := cherish_RMX1921
+PRODUCT_MODEL := Realme XT
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX1921-user 11 RKQ1.201217.002 1626947099367 release-keys" \
+    PRODUCT_NAME="RMX1921"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := realme/RMX1921/RMX1921:11/RKQ1.201217.002/1626947099367:user/release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+ro.cherish.maintainer=αиυנвαяωαℓ(anuj_barwal17)
